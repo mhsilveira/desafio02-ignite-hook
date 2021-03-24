7a45e3b76337db425e2daa10fbab4066e85d2e2d
Matheus <blanchimaah@gmail.com>
2021-03-23 22:03:20 -0300

    Primeiro commit, 10 testes passados

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -21,28 +21,65 @@ interface CartContextData {
 
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
+const qtdInStock = async (productId: number) => {
+  const { amount } = await api.get<Stock>(`/stock/${productId}`).then(response => response.data);
+  return amount;
+}
+
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
-
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    if(storagedCart){
+      return JSON.parse(storagedCart)
+    }
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const alreadyInCart   = cart.find(product => product.id === productId);
+      const productQuantityInStock = await qtdInStock(productId);
+      
+      if(!alreadyInCart){
+        // se não estiver, dá fetch no produto e adiciona no carrinho
+        // verificar no estoque se existe essa quantidade do produto tbm
+        if(productQuantityInStock > 0){
+          const product = await api.get(`products/${productId}`).then(response => response.data);
+          // adiciona a qtd ao produto, e então adiciona isso ao carrinho
+          setCart([
+            ...cart,
+            {...product, amount: 1}
+          ])
+
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));        
+        }else{
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+      }else{
+        if(alreadyInCart.amount <= productQuantityInStock){
+          // pega o item do carrinho com o id passado no metodo e incrementa em um a qtd
+          const updateCart = cart.map(item => item.id === productId ? {
+            ...item,
+            amount: Number(item.amount)+1
+          }: item);
+
+          setCart(updateCart);
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCart));
+        }else{
+          toast.error('Quantidade solicitada fora de estoque');
+          return;       
+        }
+      }
+
+    } catch(err) {
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      
     } catch {
       // TODO
     }
